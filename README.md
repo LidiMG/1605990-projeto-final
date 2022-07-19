edf37f7673c807d7a6584a278608e2b2fe86a83c
Lidiane M.  Gomes <lidigomes@gmail.com>
2022-07-18 22:05:00 -0300

    Aula1: repository, ReactApp, Bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # 1605990-projeto-final
 Final project of Target Trust Full Stack formation 2022/1.
+
+
+Steps:
+
+[x] Create repository
+
+[x] Create React repository
+
+[ ] Criar o componente de Listagem
+
+[ ] Criar o componente de Menu
+
+[ ] Criar a camada de conexão com a API
+
+[ ] Conectar com a API
+
+[ ] Listar os Departamentos na UI
---
+++ crud/src/App.js
@@ -0,0 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <h3>Departamentos</h3>
+
+      <table className='table table-striped'>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Sigla</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Recursos Humanos</td>
+            <td>RH</td>
+            <td>
+              <button className='btn btn-outline-warning'>Editar</button>
+              <button className='btn btn-outline-danger'>Excluir</button>
+            </td>
+          </tr>
+        
+          
+        </tbody>
+
+      </table>
+
+    </div>
+  );
+}
+
+export default App;
